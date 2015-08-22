e2f3266b21c83a7152ada4bd2652f721e339eec0
kevindoveton <kevindoveton@me.com>
2015-08-22 18:51:55 +0930

    added header, footer and converted index.html to index.php

---
+++ site/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Goodwood Lumberyard</title>
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+	<div class="wrapper">
+		
+		<!-- Header -->
+		<?php include_once('header.php'); ?>
+		<!-- End Header -->
+		
+		<!-- Body -->
+		<div class="row">
+			<div class="six rows"	
+				
+			</div>
+			
+			<div class="six rows">
+			<p> hello</p>
+			</div>
+		</div>
+		<!-- End Body -->
+		
+		<!-- Footer -->
+		<?php include_once('footer.php'); ?>
+		<!-- End Footer -->
+		
+	</div>
+	
+	
+</body>
+</html>
