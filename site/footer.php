c6032a368deb2df3e5a4d040a9cb43348536c4b5
kevindoveton <kevindoveton@me.com>
2015-08-24 20:00:30 +0930

    updated footer

--- site/footer.php
+++ site/footer.php
@@ -4,4 +4,4 @@ date_default_timezone_set("Australia/Adelaide");
 
 
 
-<p class="tCenter">COMP1111 || Goodwood Lumberyard || &copy <?php echo date('Y'); ?></p>
+<p class="tCenter tWhite">COMP1111 || Goodwood Lumberyard || &copy <?php echo date('Y'); ?></p>
