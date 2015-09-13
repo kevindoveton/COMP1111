beae86ff57e60d77bfc343363f08f329718d358e
kevindoveton <kevindoveton@me.com>
2015-09-13 12:13:51 +0930

    added sitemap

---
+++ site/sitemap/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+
+<!-- Start Head -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="We have all your pergola, decking, fencing need" />
+<meta name="keywords" content="goodwood, lumberyard, pergola, decking, fencing, adelaide, lumber, timber" />
+<title>Sitemap Lumberyard</title>
+
+<?php include_once('../resources/imports.php'); ?>
+
+</head> <!-- End Head -->
+
+<!-- Start Body -->
+<body>
+	<!-- Header -->
+	<?php include_once('../header.php'); ?>
+	<!-- End Header -->
+	
+	<div class="wrapper">
+		<!-- Body -->
+		<div class="row">
+			<div class="12u">
+				<h1 class="tCenter bBottom tWhite">Sitemap</h1>
+			</div>
+			
+			<div class="12u">
+				<p class="tWhite">
+					<a href="../">Home</a><br>
+					<a href="../about/">About</a><br>
+					<a href="../contact/">Contact</a><br>
+					<a href="../products/">Products</a><br>
+					<a href="../quote/">Request a Quote</a><br>
+					<a href="./">Sitemap</a>
+				</p>
+			</div>
+		</div>
+
+		<!-- End Body -->
+	</div> <!-- End Wrapper -->
+	
+	<!-- Footer -->
+	<?php include_once('../footer.php'); ?>
+	<!-- End Footer -->
+	
+	
+</body>
+<!-- End Body -->
+
+<!-- Javascript -->
+<script>
+</script>
+
+
+
+</html>
