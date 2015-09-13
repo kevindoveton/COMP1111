252f47219e4897b5af9a82d6493cc13cc1b1176f
kevindoveton <kevindoveton@me.com>
2015-09-13 22:21:16 +0930

    added 404 page

---
+++ site/404/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+
+<!-- Start Head -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="We have all your pergola, decking, fencing need" />
+<meta name="keywords" content="goodwood, lumberyard, pergola, decking, fencing, adelaide, lumber, timber" />
+<title>Products || Goodwood Lumberyard</title>
+
+<?php include_once('../resources/imports.php'); ?>
+
+</head> <!-- End Head -->
+
+<!-- Start Body -->
+<body>
+	
+	<!-- Header -->
+	<?php include_once('../header.php'); ?>
+	<!-- End Header -->
+	
+	<div class="wrapper tWhite">
+	
+		<!-- Body -->
+		<div class="row">
+			<div class="12u">
+				<h1 class="tCenter bBottom">Error</p>			
+			</div>
+		</div>
+		
+		<div class="row">
+			<p class="tCenter tWhite">Error!</p>
+		</div>
+		<!-- End Body -->
+			
+	</div> <!-- End Wrapper -->
+
+	<!-- Footer -->
+	<?php include_once('../footer.php'); ?>
+	<!-- End Footer -->	
+	
+</body>
+<!-- End Body -->
+
+</html>
