93e675607011ac8457156215b69c557af399e1d0
kevindoveton <kevindoveton@me.com>
2015-09-14 11:23:48 +0930

    Correct order of footer

--- site/footer.php
+++ site/footer.php
@@ -4,6 +4,6 @@ date_default_timezone_set("Australia/Adelaide");
 
 
 <div class="tCenter tWhite">
-	<p><a href="/">Home</a> || <a class="noDecoration" href="/about/">About</a> || <a href="/contact/">Contact</a> || <a href="/products/">Products</a> || <a href="/quote/">Request a Quote</a> || <a href="/sitemap">Sitemap</a></p>
+	<p><a href="/">Home</a> || <a href="/products/">Products</a> || <a href="/quote/">Request a Quote</a> || <a class="noDecoration" href="/about/">About</a> || <a href="/contact/">Contact</a> || <a href="/sitemap">Sitemap</a></p>
 	<p class="pTop">COMP1111 || Goodwood Lumberyard || &copy <?php echo date('Y'); ?></p>
 </div>
